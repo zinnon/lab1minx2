c2a5a98fda03a6b5e7b411e032e81e5ee325a1e7
6604062610292 <s6604062610292@email.kmutnb.ac.th>
2024-11-26 15:02:41 +0700

    Add files via upload
    3

--- lab1minx2.c
+++ lab1minx2.c
@@ -1,14 +1,20 @@
 #include <stdio.h>
 
 int main(){
-	int A;
-	int B;
+	int n1;
+	int n2;
 	printf("Type your number:");
-	scanf("%d",&A);
-	scanf("%d",&B);
-	if (A>B){
-		printf("%d is greater than %d",A,B);
+	scanf("%d",&n1);
+	scanf("%d",&n2);
+	if (n1>n2){
+		printf("%d is greater than %d",n1,n2);
+	 } else if(n2>n1){
+		printf("%d is greater than %d",n2,n1);
+	}else{
+		printf("%d is equal to %d",n1,n2);
 	}
+	
+	 
 
 	return 0;
 }
