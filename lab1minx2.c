c7955d06fd6d8dfefa34251b58efef5edf20a149
6604062610292 <s6604062610292@email.kmutnb.ac.th>
2024-12-03 13:21:56 +0700

    4

--- lab1minx2.c
+++ lab1minx2.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 
 int main(){
+	int A;
+	int B;
 	int n1;
 	int n2;
-	printf("Type your number:");
-	scanf("%d",&n1);
+printf("Type your number:");
+	scanf("%d\n",&n1);
 	scanf("%d",&n2);
 	if (n1>n2){
 		printf("%d is greater than %d",n1,n2);
@@ -12,9 +14,9 @@ int main(){
 		printf("%d is greater than %d",n2,n1);
 	}else{
 		printf("%d is equal to %d",n1,n2);
-	}
+}
 	
 	 
 
-	return 0;
+return 0;
 }
